e7b2b89bff2c56d6b9696e3d3483f2d0f224a938
fanpeichao <fan552426811@outlook.com>
2023-02-14 16:29:30 +0800

    抽抽ä¸重构代码

--- src/main/java/com/github/hcsp/io/Main.java
+++ src/main/java/com/github/hcsp/io/Main.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.io;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -18,43 +19,57 @@ import java.util.List;
 import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    private static Boolean isInterestLink(String link) {
+        return link.contains("sina.cn") && !link.contains("passport.sina.cn") && (link.contains("news.sina.cn") || "https://sina.cn".equals(link));
+    }
+
+    private static HttpGet generateATagRequest(String link) {
+        if (link.startsWith("//")) {
+            link = "https:" + link;
+        }
+        HttpGet httpGet = new HttpGet(link);
+        httpGet.addHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36");
+        return httpGet;
+    }
+
+    private static void handleArticle(Document doc) {
+        ArrayList<Element> articleTags = doc.select("article");
+        if (!articleTags.isEmpty()) {
+            for (Element articleTag : articleTags) {
+                String title = articleTag.child(0).text();
+                System.out.println(title);
+            }
+        }
+    }
+
+    private static void handleRequest(String link, List<String> linkPool, Set<String> processedLinks) {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = generateATagRequest(link);
+        try (CloseableHttpResponse response1 = httpClient.execute(httpGet)) {
+            HttpEntity entity1 = response1.getEntity();
+            String html = EntityUtils.toString(entity1);
+            Document doc = Jsoup.parse(html);
+            doc.select("a").stream().map(aTag -> aTag.attr("href")).forEach(linkPool::add);
+            handleArticle(doc);
+            processedLinks.add(link);
+        } catch (ClientProtocolException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
         List<String> linkPool = new ArrayList<>();
         Set<String> processedLinks = new HashSet<>();
         linkPool.add("https://sina.cn");
-        while (true) {
-            if (linkPool.isEmpty()) {
-                break;
-            }
+        while (!linkPool.isEmpty()) {
             String link = linkPool.remove(linkPool.size() - 1);
             if (processedLinks.contains(link)) {
                 continue;
             }
-            if (link.contains("sina.cn") && !link.contains("passport.sina.cn") && (link.contains("news.sina.cn") || "https://sina.cn".equals(link))) {
-                CloseableHttpClient httpClient = HttpClients.createDefault();
-                if (link.startsWith("//")) {
-                    link = "https:" + link;
-                }
-                HttpGet httpGet = new HttpGet(link);
-                httpGet.addHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36");
-                System.out.println(link);
-                try (CloseableHttpResponse response1 = httpClient.execute(httpGet)) {
-                    HttpEntity entity1 = response1.getEntity();
-                    String html = EntityUtils.toString(entity1);
-                    Document doc = Jsoup.parse(html);
-                    ArrayList<Element> links = doc.select("a");
-                    for (Element aTag : links) {
-                        linkPool.add(aTag.attr("href"));
-                    }
-                    ArrayList<Element> articleTags = doc.select("article");
-                    if (!articleTags.isEmpty()) {
-                        for (Element articleTag : articleTags) {
-                            String title = articleTag.child(0).text();
-                            System.out.println(title);
-                        }
-                    }
-                    processedLinks.add(link);
-                }
+            if (isInterestLink(link)) {
+                handleRequest(link, linkPool, processedLinks);
             }
         }
     }
