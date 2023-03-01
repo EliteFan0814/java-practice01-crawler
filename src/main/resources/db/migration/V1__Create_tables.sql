c43917ad74a6d64a706c13f95a979ae571db6eae
fanpeichao <fan552426811@outlook.com>
2023-03-01 11:49:47 +0800

    将数据库修改为mysql

--- src/main/resources/db/migration/V1__Create_tables.sql
+++ src/main/resources/db/migration/V1__Create_tables.sql
@@ -1,11 +1,12 @@
-create table news
+ALTER DATABASE news CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+create table NEWS
 (
     id          bigint primary key auto_increment,
     title       text,
     content     text,
     url         varchar(1000),
-    created_at  timestamp,
-    modified_at timestamp,
+    created_at  timestamp default now(),
+    modified_at timestamp default now()
 );
 
 create table LINKS_TO_BE_PROCESSED
